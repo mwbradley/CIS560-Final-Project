5e6a9908740061f5282367b67519ab3bc728dce6
Mikey Bradley <mwbradley@ksu.edu>
2026-04-19 13:57:39 -0500

    Backend

---
+++ backend/app/db.py
@@ -0,0 +1,17 @@
+import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    conn = pyodbc.connect(
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={os.getenv('DB_SERVER')};"
+        f"DATABASE={os.getenv('DB_NAME')};"
+        f"UID={os.getenv('DB_USER')};"
+        f"PWD={os.getenv('DB_PASSWORD')};"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SET search_path = FantasyFootball") 
+    return conn
