c04c343a72a888627fb8caffd91e02bf047882da
Mikey Bradley <mwbradley@ksu.edu>
2026-04-24 13:18:49 -0500

    New changes

--- backend/app/routes/players.py
+++ backend/app/routes/players.py
@@ -2,14 +2,33 @@ from db import execute_query
 from flask import Blueprint, jsonify
 
 players_bp = Blueprint("players", __name__)
+players_oldest_to_score_bp = Blueprint("playersAge", __name__)
 
 # Get all players across all leagues and teams
 @players_bp.route("/", methods=["GET"])
 def get_players():
-    players = execute_query("""SELECT P.PlayerName, P.BirthDate, P.Position
+    players = execute_query("""SELECT P.PlayerID, P.PlayerName,  
+                                 (YEAR(SYSDATETIMEOFFSET()) - YEAR(P.BirthDate)) AS PlayerAge, 
+                                 P.Position
                              FROM FantasyFootball.Player P
                                 INNER JOIN FantasyFootball.TeamPlayer TP ON TP.PlayerID = P.PlayerID
                                 INNER JOIN FantasyFootball.TeamSeason TS ON TS.TeamSeasonID = TP.TeamSeasonID
                                 INNER JOIN FantasyFootball.Season S ON S.SeasonID = TS.SeasonID
                              ORDER BY P.PlayerName ASC""")
     return jsonify(players)
+
+
+@players_oldest_to_score_bp.route("/oldest-scorer", methods=["GET"])
+def get_oldest_player():
+    playersAge = execute_query("""SELECT P.PlayerName,
+                                    MAX((YEAR(SYSDATETIMEOFFSET()) - YEAR(P.BirthDate))) AS PlayerAge,
+                                    SUM(PM.Goals) AS TotalGoals
+                                  FROM FantasyFootball.Player P
+                                    INNER JOIN FantasyFootball.TeamPlayer TP ON TP.PlayerID = P.PlayerID
+                                    INNER JOIN FantasyFootball.PlayerMatch PM ON PM.TeamPlayerID = TP.TeamPlayerID
+                                    INNER JOIN FantasyFootball.TeamSeason TS ON TS.TeamSeasonID = TP.TeamSeasonID
+                                    INNER JOIN FantasyFootball.Season S ON S.SeasonID = TS.SeasonID
+                                  GROUP BY P.PlayerName, (YEAR(SYSDATETIMEOFFSET()) - YEAR(P.BirthDate)), PM.Goals
+                                  ORDER BY PM.Goals DESC
+   """)
+    return jsonify(playersAge)
--- backend/app/routes/teams.py
+++ backend/app/routes/teams.py
@@ -1,7 +1,8 @@
 from db import execute_query
 from flask import Blueprint, jsonify
 
-teams_bp = Blueprint("team", __name__)
+teams_bp = Blueprint("teamPlayer", __name__)
+teams_redcard_bp = Blueprint("num_red_cards", __name__)
 
 @teams_bp.route("/<teamName>", methods=["GET"])
 def get_players_team(teamName):
@@ -13,3 +14,16 @@ def get_players_team(teamName):
                                   WHERE T.TeamName = ?                              
     """, (teamName,))
     return jsonify(teamPlayer)
+
+@teams_redcard_bp.route("/red-cards", methods=["GET"])
+def get_num_redcards():
+    num_red_cards = execute_query("""SELECT T.TeamName, S.SeasonName,
+                                      SUM(PM.RedCards) OVER(PARTITION BY T.TeamName) AS TotalTeamRedCards
+                                     FROM FantasyFootball.PlayerMatch PM
+                                      INNER JOIN FantasyFootball.TeamPlayer TP ON TP.TeamPlayerID = PM.TeamPlayerID
+                                      INNER JOIN FantasyFootball.TeamSeason TS ON TS.TeamSeasonID = TP.TeamSeasonID
+                                      INNER JOIN FantasyFootball.Team T ON T.TeamID = TS.TeamID
+                                      INNER JOIN FantasyFootball.Season S ON S.SeasonID = TS.SeasonID
+                                     GROUP BY T.TeamName, S.SeasonName, PM.RedCards
+    """)
+    return jsonify(num_red_cards)
