b35ea4dd100290ea65fd9c73070051bd4e233f4f
Mikey Bradley <mwbradley@ksu.edu>
2026-04-24 13:16:35 -0500

    Added more sql queries

--- database/Aggregating_Queries.sql
+++ database/Aggregating_Queries.sql
@@ -32,3 +32,24 @@ FROM FantasyFootball.Player P
 	INNER JOIN FantasyFootball.Team T ON T.TeamID = TS.TeamID
 WHERE P.PlayerName = N'Erling Haaland'
 GROUP BY P.PlayerName, T.TeamName
+
+SELECT T.TeamName, S.SeasonName,
+	SUM(PM.RedCards) OVER(PARTITION BY T.TeamName) AS TotalTeamRedCards
+FROM FantasyFootball.PlayerMatch PM
+	INNER JOIN FantasyFootball.TeamPlayer TP ON TP.TeamPlayerID = PM.TeamPlayerID
+	INNER JOIN FantasyFootball.TeamSeason TS ON TS.TeamSeasonID = TP.TeamSeasonID
+	INNER JOIN FantasyFootball.Team T ON T.TeamID = TS.TeamID
+	INNER JOIN FantasyFootball.Season S ON S.SeasonID = TS.SeasonID
+GROUP BY T.TeamName, S.SeasonName, PM.RedCards
+
+SELECT TOP(1)
+	P.PlayerName,
+	MAX((YEAR(SYSDATETIMEOFFSET()) - YEAR(P.BirthDate))) AS PlayerAge,
+	SUM(PM.Goals) AS TotalGoals
+FROM FantasyFootball.Player P
+	INNER JOIN FantasyFootball.TeamPlayer TP ON TP.PlayerID = P.PlayerID
+	INNER JOIN FantasyFootball.PlayerMatch PM ON PM.TeamPlayerID = TP.TeamPlayerID
+	INNER JOIN FantasyFootball.TeamSeason TS ON TS.TeamSeasonID = TP.TeamSeasonID
+	INNER JOIN FantasyFootball.Season S ON S.SeasonID = TS.SeasonID
+GROUP BY P.PlayerName, P.BirthDate, PM.Goals
+ORDER BY TotalGoals DESC
